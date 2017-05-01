ab487c130e428e28277629f149e7830a35c5e6c2
Drafter500 <vzemtsov@yahoo.com>
2017-05-01 22:19:03 +0300

    multiple balls

--- src/index.js
+++ src/index.js
@@ -34,13 +34,13 @@ function ready() {
       this.vy += acceleration;
     }
 
-    bounceX(speedAbsorption) {
-      this.vx = getSpeedAfterBounce(this.vx, speedAbsorption);
+    bounceX(absorptionCoefficient) {
+      this.vx = getSpeedAfterBounce(this.vx, absorptionCoefficient);
     }
 
-    bounceY(speedAbsorption) {
-      this.vy = getSpeedAfterBounce(this.vy, speedAbsorption);
-      this.vx *= speedAbsorption;
+    bounceY(absorptionCoefficient) {
+      this.vy = getSpeedAfterBounce(this.vy, absorptionCoefficient);
+      this.vx *= absorptionCoefficient;
 
       if (Math.abs(this.vy) < 0.01) {
         this.vy = 0;
@@ -56,31 +56,44 @@ function ready() {
     }
   }
 
-  const ball = new Ball(20, 'red');
+  const ball1 = new Ball(20, 'red');
+  const ball2 = new Ball(20, 'green');
+  const ball3 = new Ball(20, 'blue');
+
+  ball2.setSpeed(-5, 0);
+  ball2.setPosition(250, 80);
+
+  ball3.setSpeed(8, 0);
+  ball3.setPosition(50, 120);
+
+  const balls = [ball1, ball2, ball3];
+
   const env = {
     ay: 0.5,
-    fadey: 0.8,
+    absorptionCoefficient: 0.8,
   };
 
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ball.draw();
-    ball.x += ball.vx;
-    ball.y += ball.vy;
+    for (const ball of balls) {
+      ball.draw();
+      ball.x += ball.vx;
+      ball.y += ball.vy;
 
-    if (ball.y + ball.vy + ball.radius > canvas.height ||
-            ball.y + ball.vy < 0) {
-      ball.bounceY(env.fadey);
+      if (ball.y + ball.vy + ball.radius > canvas.height ||
+              ball.y + ball.vy < 0) {
+        ball.bounceY(env.absorptionCoefficient);
 
-      statusPanel.innerHTML = `vy = ${Math.abs(ball.vy)}`;
-    } else {
-      ball.accelerateY(env.ay);
-    }
+        //statusPanel.innerHTML = `vy = ${Math.abs(ball.vy)}`;
+      } else {
+        ball.accelerateY(env.ay);
+      }
 
-    if (ball.x + ball.vx + ball.radius > canvas.width ||
-            ball.x + ball.vx - ball.radius < 0) {
-      ball.bounceX(env.fadey);
-    }
+      if (ball.x + ball.vx + ball.radius > canvas.width ||
+              ball.x + ball.vx - ball.radius < 0) {
+        ball.bounceX(env.absorptionCoefficient);
+      }
+    };
 
     raf = window.requestAnimationFrame(draw);
   }
